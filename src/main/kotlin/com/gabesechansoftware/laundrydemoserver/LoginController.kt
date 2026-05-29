d1d3301bdbddd804c3b50b246bb7fdd521f0d76e
gsechan <gsechan@hotmail.com>
2026-05-29 16:26:59 -0400

    Make user nullable for failure case

--- src/main/kotlin/com/gabesechansoftware/laundrydemoserver/LoginController.kt
+++ src/main/kotlin/com/gabesechansoftware/laundrydemoserver/LoginController.kt
@@ -2,14 +2,13 @@ package com.gabesechansoftware.laundrydemoserver
 
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 data class LoginRequest(val username: String, val password: String)
 
 data class LoginResponse(
     val success: Boolean,
-    val user: LoginUser
+    val user: LoginUser?
 )
 
 data class LoginUser(
@@ -24,6 +23,10 @@ class LoginController {
     @PostMapping("/login")
     fun login(
         @RequestBody request: LoginRequest): LoginResponse {
-        return LoginResponse(true, LoginUser("1","Gabe", "",""))
+        if(request.username == "Gabe" && request.password == "1234")
+            return LoginResponse(true, LoginUser("1","Gabe", "",""))
+        else {
+            return LoginResponse(false, null)
+        }
     }
 }
