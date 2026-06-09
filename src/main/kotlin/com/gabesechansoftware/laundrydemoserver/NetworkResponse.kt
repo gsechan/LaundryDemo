23d48964b11a6f624965ffaaa9cc57c76f5eff72
gsechan <gsechan@hotmail.com>
2026-06-09 15:56:50 -0400

    Make error in network result a list

--- src/main/kotlin/com/gabesechansoftware/laundrydemoserver/NetworkResponse.kt
+++ src/main/kotlin/com/gabesechansoftware/laundrydemoserver/NetworkResponse.kt
@@ -5,7 +5,8 @@ enum class NetworkErrorType{
     BAD_AUTH,
     API_SPECIFIC_ERROR,
 }
-data class NetworkResponse<T>(val success: Boolean, val errorType: String, val error: String?, val data: T?) {
-    constructor(data:T) : this(true, NetworkErrorType.NONE.toString(), null, data)
-    constructor(errorType: NetworkErrorType, error: String): this(false, errorType.toString(), error, null)
+data class NetworkResponse<T>(val success: Boolean, val errorType: String, val errors: List<String>, val data: T?) {
+    constructor(data:T) : this(true, NetworkErrorType.NONE.toString(), emptyList(), data)
+    constructor(errorType: NetworkErrorType, error: String): this(false, errorType.toString(), listOf(error), null)
+    constructor(errorType: NetworkErrorType, errors: List<String>): this(false, errorType.toString(), errors, null)
 }
